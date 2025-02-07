76804c84d4502f6b88d8464fe88e0cf26f65a649
Andy Gifford <ajgifford@users.noreply.github.com>
2025-02-07 08:08:18 -0600

    Add pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'keepwatching-server',
+      script: 'dist/index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'keepwatching-server-dev',
+      script: 'yarn',
+      args: 'dev',
+      exec_mode: 'fork',
+      watch: ['src'],
+      ignore_watch: ['node_modules', 'dist'],
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
