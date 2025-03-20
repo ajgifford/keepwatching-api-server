30282b642391b529ebb221f608efcc0ce4097969
Andy Gifford <ajgifford@users.noreply.github.com>
2025-03-20 11:09:14 -0500

    Refactor search controller to reduce code duplication

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,102 +1,68 @@
-import { cliLogger } from '../logger/logger';
 import { SearchParams } from '../schema/searchSchema';
+import { DiscoverAndSearchResult } from '../types/discoverAndSearchTypes';
 import { axiosTMDBAPIInstance } from '../utils/axiosInstance';
 import { generateGenreArrayFromIds } from '../utils/genreUtility';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import NodeCache from 'node-cache';
 
 const cache = new NodeCache({ stdTTL: 300 });
 
+enum MediaType {
+  SHOW = 'tv',
+  MOVIE = 'movie',
+}
+
 // GET /api/v1/search/shows
-export const searchShows = async (req: Request, res: Response): Promise<void> => {
-  const { searchString, year, page = 1 } = req.query as unknown as SearchParams;
-
-  const cacheKey = `show_search_${searchString}_${year}_${page}`;
-  const cachedData = cache.get(cacheKey);
-
-  if (cachedData) {
-    res.status(200).json(cachedData);
-    return;
-  }
-
-  try {
-    const response = await axiosTMDBAPIInstance.get('/search/tv', {
-      params: {
-        query: searchString,
-        page,
-        first_air_date_year: year,
-        include_adult: false,
-        language: req.headers['accept-language'] || 'en-US',
-      },
-    });
-    const results: any[] = response.data.results;
-    const searchResult = results.map((result) => {
-      return {
-        id: result.id,
-        title: result.name,
-        genres: generateGenreArrayFromIds(result.genre_ids),
-        premiered: result.first_air_date,
-        summary: result.overview,
-        image: result.poster_path,
-        rating: result.vote_average,
-        popularity: result.popularity,
-      };
-    });
-
-    const responseData = {
-      results: searchResult,
-      total_pages: response.data.total_pages,
-      total_results: response.data.total_results,
-      current_page: page,
-    };
-
-    cache.set(cacheKey, responseData);
-    res.status(200).json(responseData);
-  } catch (error) {
-    cliLogger.error('Error in shows search:', error);
-    res.status(500).json({
-      message: 'Unexpected error while searching for shows',
-      error: error instanceof Error ? error.message : 'Unknown error',
-    });
-  }
+export const searchShows = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  await searchMedia(req, res, next, MediaType.SHOW);
 };
 
 // GET /api/v1/search/movies
-export const searchMovies = async (req: Request, res: Response) => {
-  const { searchString, year, page = 1 } = req.query as unknown as SearchParams;
+export const searchMovies = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  await searchMedia(req, res, next, MediaType.MOVIE);
+};
 
-  const cacheKey = `movie_search_${searchString}_${year}_${page}`;
-  const cachedData = cache.get(cacheKey);
-
-  if (cachedData) {
-    res.status(200).json(cachedData);
-    return;
-  }
-
+async function searchMedia(req: Request, res: Response, next: NextFunction, mediaType: MediaType): Promise<void> {
   try {
-    const response = await axiosTMDBAPIInstance.get('/search/movie', {
-      params: {
-        query: searchString,
-        page,
-        primary_release_year: year,
-        include_adult: false,
-        region: 'US',
-        language: req.headers['accept-language'] || 'en-US',
-      },
-    });
+    const { searchString, year, page = 1 } = req.query as unknown as SearchParams;
+
+    const mediaTypeName = mediaType === MediaType.SHOW ? 'show' : 'movie';
+    const cacheKey = `${mediaTypeName}_search_${searchString}_${year}_${page}`;
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+      res.status(200).json(cachedData);
+      return;
+    }
+
+    const params: Record<string, any> = {
+      query: searchString,
+      page,
+      include_adult: false,
+      language: req.headers['accept-language'] || 'en-US',
+    };
+
+    if (mediaType === MediaType.SHOW) {
+      params.first_air_date_year = year;
+    } else {
+      params.primary_release_year = year;
+      params.region = 'US';
+    }
+
+    const response = await axiosTMDBAPIInstance.get(`/search/${mediaType}`, { params });
 
     const results: any[] = response.data.results;
     const searchResult = results.map((result) => {
       return {
         id: result.id,
-        title: result.title,
+        title: mediaType === MediaType.MOVIE ? result.title : result.name,
         genres: generateGenreArrayFromIds(result.genre_ids),
-        premiered: result.release_date,
+        premiered: mediaType === MediaType.MOVIE ? result.release_date : result.first_air_date,
         summary: result.overview,
         image: result.poster_path,
         rating: result.vote_average,
         popularity: result.popularity,
-      };
+      } as DiscoverAndSearchResult;
     });
 
     const responseData = {
@@ -109,10 +75,6 @@ export const searchMovies = async (req: Request, res: Response) => {
     cache.set(cacheKey, responseData);
     res.status(200).json(responseData);
   } catch (error) {
-    cliLogger.error('Error in movie search:', error);
-    res.status(500).json({
-      message: 'Unexpected error while searching for movies',
-      error: error instanceof Error ? error.message : 'Unknown error',
-    });
+    next(error);
   }
-};
+}
