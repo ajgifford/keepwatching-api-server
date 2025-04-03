716973749559a22b3e332041c57fc82883496520
Andy Gifford <ajgifford@users.noreply.github.com>
2025-04-02 21:16:45 -0500

    Update the logics to determine the directory and file name for uploads

--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -12,27 +12,35 @@ if (!fs.existsSync(UPLOADS_DIR)) {
   fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 }
 
+if (!fs.existsSync(UPLOADS_DIR + '/accounts')) {
+  fs.mkdirSync(UPLOADS_DIR + '/accounts', { recursive: true });
+}
+if (!fs.existsSync(UPLOADS_DIR + '/profiles')) {
+  fs.mkdirSync(UPLOADS_DIR + '/profiles', { recursive: true });
+}
+
 const maxSize: number = 2 * 1024 * 1024;
 
 const storage: StorageEngine = multer.diskStorage({
   destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
     let destinationPath;
-    if (req.path.startsWith('/api/v1/upload/accounts/')) {
-      destinationPath = UPLOADS_DIR + '/accounts';
-    } else if (req.path.startsWith('/api/v1/upload/profiles/')) {
+    if (req.path.includes('/profiles/')) {
       destinationPath = UPLOADS_DIR + '/profiles';
     } else {
-      destinationPath = UPLOADS_DIR;
+      destinationPath = UPLOADS_DIR + '/accounts';
     }
     cb(null, destinationPath);
   },
   filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-    const { id } = req.params;
-    let fileName = buildAccountImageName(id, file.mimetype);
-    if (req.path.startsWith('/api/v1/upload/profiles/')) {
-      fileName = buildProfileImageName(id, file.mimetype);
+    if (req.path.includes('/profiles/')) {
+      const { profileId } = req.params;
+      const fileName = buildProfileImageName(profileId, file.mimetype);
+      cb(null, fileName);
+    } else {
+      const { accountId } = req.params;
+      const fileName = buildAccountImageName(accountId, file.mimetype);
+      cb(null, fileName);
     }
-    cb(null, fileName);
   },
 });
 
