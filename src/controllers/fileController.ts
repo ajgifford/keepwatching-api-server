05243840c738c0ba298e4ba22afaa1bd17e31b90
Andy Gifford <ajgifford@users.noreply.github.com>
2025-06-10 15:17:25 -0500

    Correct the property name for the updateProfileImage return type

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -64,7 +64,7 @@ export const uploadProfileImage = asyncHandler(async (req: Request, res: Respons
         if (updatedProfile) {
           res.status(200).send({
             message: `Uploaded the file successfully: ${profileImage}`,
-            result: updatedProfile,
+            profile: updatedProfile,
           });
           const filePath = getUploadDirectory() + '/profiles/' + profile.image;
           fs.unlink(filePath, (err) => {
