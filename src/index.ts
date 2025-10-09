2e9e94345995da96760cc943834146d96af72962
Andy Gifford <ajgifford@users.noreply.github.com>
2025-10-09 10:54:03 -0500

    Add a middleware to track account activity

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,6 @@ import {
   getAppVersion,
   getCertsKeyPath,
   getCertsServerPath,
-  getEmailConfig,
   getLogDirectory,
   getPort,
   getRateLimitMax,
@@ -34,6 +33,7 @@ import {
 } from '@ajgifford/keepwatching-common-server/config';
 import { ErrorMessages, appLogger, cliLogger } from '@ajgifford/keepwatching-common-server/logger';
 import { responseInterceptor } from '@ajgifford/keepwatching-common-server/middleware';
+import { trackUserActivity } from '@ajgifford/keepwatching-common-server/middleware';
 import { databaseService, emailService, socketService } from '@ajgifford/keepwatching-common-server/services';
 import { initScheduledJobs, shutdownJobs } from '@ajgifford/keepwatching-common-server/services';
 import {
@@ -175,6 +175,7 @@ app.get('/health', (req: Request, res: Response) => {
 });
 
 app.use(accountRouter);
+app.use(trackUserActivity);
 app.use(authenticateUser, profileRouter);
 app.use(authenticateUser, searchRouter);
 app.use(authenticateUser, discoverRouter);
